bd869cdd27ff618f84d9d1d70844d95ae852ef22
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2013-02-13 21:19:27 +0000

    adding the separated job core

---
+++ src/main/groovy/metridoc/core/Job.groovy
@@ -0,0 +1,12 @@
+package metridoc.core
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tbarker
+ * Date: 2/13/13
+ * Time: 4:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Job {
+    def execute()
+}
---
+++ src/main/groovy/metridoc/core/MetridocJob.groovy
@@ -0,0 +1,11 @@
+package metridoc.core
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tbarker
+ * Date: 2/13/13
+ * Time: 4:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+abstract class MetridocJob implements Job {
+}
