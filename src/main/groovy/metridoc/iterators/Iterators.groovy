e45775f477115f3cadf6fb8b9527b49099770ec9
Tommy Barker <mingus.karate@gmail.com>
2013-07-05 15:18:56 -0400

    move IteratorTool stuff to Iterators util

--- src/main/groovy/metridoc/iterators/Iterators.groovy
+++ src/main/groovy/metridoc/iterators/Iterators.groovy
@@ -1,10 +1,27 @@
 package metridoc.iterators
 
+import metridoc.writers.*
+
 /**
  * Created with IntelliJ IDEA on 5/31/13
  * @author Tommy Barker
  */
 class Iterators {
+
+    public static final ITERATORS = [
+            delimited: DelimitedLineIterator,
+            sql: SqlIterator,
+            xls: XlsIterator,
+            xlsx: XlsxIterator,
+            csv: CsvIterator
+    ]
+
+    public static final WRITERS = [
+            sql: DataSourceIteratorWriter,
+            table: TableIteratorWriter,
+            entity: EntityIteratorWriter,
+            split: SplitIteratorWriter
+    ]
 
     static RowIterator toRowIterator(Iterator<Map> iterator) {
         assert iterator: "iterator must not be null or empty"
@@ -47,4 +64,42 @@ class Iterators {
                 transformer: transformer
         )
     }
+
+    @SuppressWarnings("GrMethodMayBeStatic")
+    static WrappedIterator createIterator(LinkedHashMap properties, Class<RowIterator> iteratorClass) {
+        def iterator = iteratorClass.newInstance(properties)
+        new WrappedIterator(iterator: iterator)
+    }
+
+    static WrappedIterator createIterator(LinkedHashMap properties, String name) {
+        createIterator(properties, ITERATORS[name] as Class<RowIterator>)
+    }
+
+    static IteratorWriter createWriter(LinkedHashMap properties, String name) {
+        createWriter(properties, WRITERS[name] as Class<IteratorWriter>)
+    }
+
+    static IteratorWriter createWriter(LinkedHashMap properties, Class<IteratorWriter> writerClass) {
+        writerClass.newInstance(properties)
+    }
 }
+
+class WrappedIterator {
+    @Delegate
+    RowIterator iterator
+
+    WriteResponse writeTo(LinkedHashMap properties, Class<IteratorWriter> writerClass) {
+        def writer = Iterators.createWriter(properties, writerClass)
+        writer.write(iterator)
+    }
+
+    WriteResponse writeTo(LinkedHashMap properties, String name) {
+        def writer = Iterators.createWriter(properties, name)
+        writer.write(iterator)
+    }
+
+    WriteResponse writeTo(String name) {
+        writeTo([:], name)
+    }
+}
+
--- src/test/groovy/metridoc/iterators/IteratorsSpec.groovy
+++ src/test/groovy/metridoc/iterators/IteratorsSpec.groovy
@@ -1,4 +1,4 @@
-package metridoc.core.tools
+package metridoc.iterators
 
 import com.google.common.collect.Table
 import org.junit.Rule
@@ -9,7 +9,7 @@ import spock.lang.Specification
  * Created with IntelliJ IDEA on 7/5/13
  * @author Tommy Barker
  */
-class IteratorToolSpec extends Specification {
+class IteratorsSpec extends Specification {
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder()
@@ -26,7 +26,7 @@ class IteratorToolSpec extends Specification {
 
     void "iterators are created via lookup"() {
         when: "a delimited iterator is built"
-        def iterator = new IteratorTool().createIterator("delimited", delimiter: /\|/, file: foo)
+        def iterator = Iterators.createIterator("delimited", delimiter: /\|/, file: foo)
         def next1 = iterator.next()
         def next2 = iterator.next()
 
@@ -49,7 +49,7 @@ class IteratorToolSpec extends Specification {
 
     void "iterators should also be able to write"() {
         when: "a delimited iterator is built"
-        def iterator = new IteratorTool().createIterator("delimited", delimiter: /\|/, file: foo)
+        def iterator = Iterators.createIterator("delimited", delimiter: /\|/, file: foo)
 
         and: "and is written to a guave table"
         def table = iterator.writeTo("table") as Table
