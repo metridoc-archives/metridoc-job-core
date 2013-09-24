cabaac67ea5fd4c2c8453c259f25d1740dfbe4c7
Tommy Barker <thomas.p.barker@gmail.com>
2013-09-23 20:23:42 -0400

    all writers close recordIterators
    also did some import cleanup

--- src/main/groovy/metridoc/iterators/IteratorsExtension.groovy
+++ src/main/groovy/metridoc/iterators/IteratorsExtension.groovy
@@ -1,9 +1,6 @@
 package metridoc.iterators
 
-import metridoc.writers.WriteResponse
-
-import javax.sql.DataSource
-import java.sql.ResultSet;
+import java.sql.ResultSet
 
 /**
  * Created with IntelliJ IDEA on 9/11/13
@@ -13,12 +10,12 @@ import java.sql.ResultSet;
 public class IteratorsExtension {
 
     public static WrappedIterator fromSql(Iterators self, ResultSet resultSet) {
-        assert resultSet != null : "resultSet cannot be null"
+        assert resultSet != null: "resultSet cannot be null"
         Iterators.createIterator([resultSet: resultSet] as LinkedHashMap, "sql")
     }
 
     public static WrappedIterator fromDelimited(Iterators self, File file, String delimiter, Map options = [:]) {
-        assert file : "file cannot be null"
+        assert file: "file cannot be null"
         def result = fromDelimited(self, file.newInputStream(), delimiter)
         result.file = file
 
@@ -26,7 +23,7 @@ public class IteratorsExtension {
     }
 
     public static WrappedIterator fromDelimited(Iterators self, InputStream stream, String delimiter, Map options = [:]) {
-        assert stream : "stream cannot be null"
+        assert stream: "stream cannot be null"
         def params = [delimiter: delimiter, inputStream: stream] as LinkedHashMap
         params.putAll(options)
         Iterators.createIterator(params, "delimited")
@@ -45,27 +42,27 @@ public class IteratorsExtension {
     }
 
     public static WrappedIterator fromXml(Iterators self, InputStream inputStream, String tag) {
-        Iterators.createIterator([inputStream: inputStream, tag:tag] as LinkedHashMap, "xml")
+        Iterators.createIterator([inputStream: inputStream, tag: tag] as LinkedHashMap, "xml")
     }
 
     public static WrappedIterator fromXml(Iterators self, File file, String tag) {
-        Iterators.createIterator([file: file, tag:tag] as LinkedHashMap, "xml")
+        Iterators.createIterator([file: file, tag: tag] as LinkedHashMap, "xml")
     }
 
     public static WrappedIterator fromXml(Iterators self, InputStream inputStream, String tag, Map inheritedNamespaces) {
-        Iterators.createIterator([inputStream: inputStream, tag:tag, inheritedNamespaces: inheritedNamespaces] as LinkedHashMap, "xml")
+        Iterators.createIterator([inputStream: inputStream, tag: tag, inheritedNamespaces: inheritedNamespaces] as LinkedHashMap, "xml")
     }
 
     public static WrappedIterator fromXml(Iterators self, File file, String tag, Map inheritedNamespaces) {
-        Iterators.createIterator([file: file, tag:tag, inheritedNamespaces: inheritedNamespaces] as LinkedHashMap, "xml")
+        Iterators.createIterator([file: file, tag: tag, inheritedNamespaces: inheritedNamespaces] as LinkedHashMap, "xml")
 
     }
 
     public static WrappedIterator fromXml(Iterators self, InputStream inputStream, String tag, Map inheritedNamespaces, String charSet) {
-        Iterators.createIterator([inputStream: inputStream, tag:tag, inheritedNamespaces: inheritedNamespaces, charSet: charSet] as LinkedHashMap, "xml")
+        Iterators.createIterator([inputStream: inputStream, tag: tag, inheritedNamespaces: inheritedNamespaces, charSet: charSet] as LinkedHashMap, "xml")
     }
 
     public static WrappedIterator fromXml(Iterators self, File file, String tag, Map inheritedNamespaces, String charSet) {
-        Iterators.createIterator([file: file, tag:tag, inheritedNamespaces: inheritedNamespaces, charSet: charSet] as LinkedHashMap, "xml")
+        Iterators.createIterator([file: file, tag: tag, inheritedNamespaces: inheritedNamespaces, charSet: charSet] as LinkedHashMap, "xml")
     }
 }
