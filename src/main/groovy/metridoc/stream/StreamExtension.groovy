b04e0dc95763380bc2135798ce8038e317689509
Tommy Barker <thomas.p.barker@gmail.com>
2013-10-22 08:55:19 -0400

    add more stream functionality
    putting all stream stuff in the stream package.  We will deprecate
    all of the Iterator and Writer api.  It was too complicated,
    this is much, much simpler.  We still need to add toBean method and
    migrate new streams for xml, csv, delimited and sql.  We will call
    these 'streams' instead of iterators.

--- src/main/groovy/metridoc/stream/StreamExtension.groovy
+++ src/main/groovy/metridoc/stream/StreamExtension.groovy
@@ -1,8 +1,9 @@
-package metridoc.iterators
+package metridoc.stream
 
 import groovy.stream.Stream
 import metridoc.writers.WriteResponse
 import metridoc.writers.WrittenRecordStat
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 /**
@@ -10,9 +11,10 @@ import org.slf4j.LoggerFactory
  */
 class StreamExtension {
 
+    @SuppressWarnings("GroovyAccessibility")
     public static WriteResponse each(Stream self, Closure closure) {
         def response = new WriteResponse()
-        Closure clone = closure.clone()
+        Closure clone = closure.clone() as Closure
 
         def wrapped = self.wrapped
         Closure oldCondition = wrapped.condition
@@ -32,11 +34,13 @@ class StreamExtension {
             }
         }
 
+        int count = 0
+        int logEvery = getMetaProperty(self, "logEvery", Integer) ?: 1000
         while (self.hasNext()) {
             def next = self.next()
             boolean valid = true
-            if(self.metaClass.respondsTo(self, "getValidator")) {
-                Closure validator = self.getValidator()
+            Closure validator = getMetaProperty(self, "validator", Closure)
+            if(validator) {
                 validator.delegate = wrapped.using
                 validator.resolveStrategy = Closure.DELEGATE_FIRST
                 try {
@@ -46,31 +50,73 @@ class StreamExtension {
                     }
                 }
                 catch (AssertionError assertionError) {
-                    LoggerFactory.getLogger(Stream).warn("invalid record: $assertionError.message")
+                    getLogger(self).warn("invalid record: $assertionError.message")
                     valid = false
                 }
             }
             if (valid) {
                 clone.call(next)
                 response.aggregateStats[WrittenRecordStat.Status.WRITTEN] = response.writtenTotal + 1
-
             }
             else {
                 response.aggregateStats[WrittenRecordStat.Status.INVALID] = response.invalidTotal + 1
+            }
+
+            if(count != 0 && count % logEvery == 0) {
+                getLogger(self).info response.toString()
             }
         }
 
         if(response.aggregateStats[WrittenRecordStat.Status.IGNORED] > 1 && lastFilterInvalid) {
             response.aggregateStats[WrittenRecordStat.Status.IGNORED] = response.ignoredTotal - 1
         }
+
+        getLogger(self).info response.toString()
         return response
     }
 
     public static Stream validate(Stream self, Closure validate) {
-        self.metaClass.getValidator = {
-            validate.clone() as Closure
+        setMetaProperty(self, "validator", validate)
+        return self
+    }
+
+    protected static <T> T getMetaProperty(Stream self, String propertyName, Class<T> aClass) {
+        initializePropertyMapIfNotThere(self)
+
+        def response = self.getMetaPropertyMap[propertyName]
+        if(response && aClass.isAssignableFrom(response.getClass())) {
+            return response.asType(aClass)
         }
 
+        return null
+    }
+
+    protected static void setMetaProperty(Stream self, String propertyName, propertyValue) {
+        initializePropertyMapIfNotThere(self)
+
+        self.getMetaPropertyMap[propertyName] = propertyValue
+    }
+
+    private static void initializePropertyMapIfNotThere(Stream self) {
+        if(!self.metaClass.respondsTo(self, "getMetaPropertyMap")) {
+            self.metaClass.metaPropertyMap = {
+                [:]
+            }
+        }
+    }
+
+    public static Stream logEvery(Stream self, int actionCountBeforeLogging) {
+        setMetaProperty(self, "logEvery", actionCountBeforeLogging)
         return self
+    }
+
+    public static Stream setLogName(Stream self, String logName) {
+        setMetaProperty(self, "logName", logName)
+        return self
+    }
+
+    public static Logger getLogger(Stream self) {
+        def logName = getMetaProperty(self, "logName", String) ?: "metridoc.stream"
+        LoggerFactory.getLogger(logName)
     }
 }
