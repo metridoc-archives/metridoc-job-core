9aac16cb671c912a4dd3438518c37183558d5d55
Tommy Barker <thomas.p.barker@gmail.com>
2013-10-21 22:46:06 -0400

    simplify iterator api
    expanding iterator api by expanding the groovy-stream library we are
    using.  once I add a coupe more bells and whistles I will deprecate
    the iterator writer library all together.  It has not proved very
    useful or intuitive.

--- src/test/groovy/metridoc/iterators/IteratorsSpec.groovy
+++ src/test/groovy/metridoc/iterators/IteratorsSpec.groovy
@@ -3,6 +3,7 @@ package metridoc.iterators
 import com.google.common.collect.Table
 import com.google.common.collect.TreeBasedTable
 import groovy.sql.Sql
+import groovy.stream.Stream
 import metridoc.utils.DataSourceConfigUtil
 import metridoc.writers.DefaultIteratorWriter
 import metridoc.writers.IteratorWriter
@@ -144,5 +145,42 @@ class IteratorsSpec extends Specification {
         catch (Throwable ignore) {
 
         }
+    }
+
+    void "test iterator mapping"() {
+        when:
+        WriteResponse result = [0,1,2,3].toStream().map {
+            it * 2
+        }.each{}
+
+        then:
+        4 == result.writtenTotal
+        4 == result.total
+    }
+
+    void "test filtering"() {
+        when:
+        WriteResponse result = [0,1,2,3].toStream().filter {
+            it % 2 == 0
+        }.each{}
+
+        then:
+        2 == result.writtenTotal
+        4 == result.total
+        2 == result.ignoredTotal
+    }
+
+    void "test validation"() {
+        when:
+        WriteResponse result = [0,1,2,3,4].toStream().filter {
+            it % 2 == 0
+        }.map{it * 2}.validate {assert it != 8}.each{}
+
+        then:
+        2 == result.writtenTotal
+        println result.toString()
+        5 == result.total
+        2 == result.ignoredTotal
+        1 == result.invalidTotal
     }
 }
